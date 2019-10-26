9478aa51b84e28a5cb416701359af32d915381be
Vincent <const@lomo.cc>
2019-10-26 21:31:22 +0800

    fix: use universal

--- api/update.js
+++ api/update.js
@@ -1,7 +1,7 @@
 module.exports = (req, res) => {
   let obj = {
     url:
-      "https://github.com/lomocc/lottery/releases/download/latest/app-armeabi-v7a-release.apk",
+      "https://github.com/lomocc/lottery/releases/download/latest/app-universal-release.apk",
     versionCode: 4,
     updateMessage: "1. 适配 Android 8.0\n2. 适配 Android 9.0\n3. 更新说明"
   };

fe55d05b34dfdca3b325a2b767bf64a6acf6b016
Vincent <const@lomo.cc>
2019-12-02 23:56:22 +0800

    upgrade: v0.0.3

--- api/checkForUpdates.js
+++ api/checkForUpdates.js
@@ -1,8 +1,8 @@
 module.exports = (req, res) => {
   let obj = {
     browser_download_url:
-      "http://github.lomo.cc/lomocc/lottery-release/releases/download/v0.0.2/app-armeabi-v7a-release.apk",
-    version_code: 1048578
+      "http://github.lomo.cc/lomocc/lottery-release/releases/download/v0.0.3/app-armeabi-v7a-release.apk",
+    version_code: 1048579
   };
   res.status(200).send(obj);
 };
